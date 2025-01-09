5b8df8064256cf173ab54727ea8a6e452bdb6b2d
fanny <renatenyatsikor@gmail.com>
2025-01-09 15:27:25 +0100

    update readme

--- app.py
+++ app.py
@@ -1,59 +1,84 @@
 from flask import Flask, render_template, url_for
 from flask import Flask
 from flask_saml2.sp  import ServiceProvider
-from flask_saml2.utils import certificate_from_file, private_key_from_file
+# from flask_saml2.utils import certificate_from_file, private_key_from_file
 
 
-class MyServiceProvider(ServiceProvider):
+# class MyServiceProvider(ServiceProvider):
 
-    def get_default_login_return_url(self):
-        return url_for('index', _external=True)
+#     def get_default_login_return_url(self):
+#         return url_for('index', _external=True)
     
-    def get_logout_return_url(self):
-         return url_for('index', _external=True)
+#     def get_logout_return_url(self):
+#          return url_for('index', _external=True)
     
 
 
 
-sp = MyServiceProvider()
+#sp = MyServiceProvider()
 
 
 app = Flask(__name__)
 
 
-app.secret_key = 'not a secret'
-app.config['SERVER_NAME'] = 'http://localhost:8080'
+# app.secret_key = 'not a secret'
+# app.config['SERVER_NAME'] = 'http://localhost:8080'
 
 
 
-app.config['SAML2_SP'] = {
-    'certificate' :certificate_from_file('sp-certificate.pem'),
-    'private_key' : private_key_from_file('sp-private-key.pem')
+# app.config['SAML2_SP'] = {
+#     'certificate' :certificate_from_file('keys\sp-certificate.pem'),
+#     'private_key' : private_key_from_file('keys\sp-private-key.pem')
 
-}
+# }
 
-app.config['SAML2_IDENTITY_PROVIDERS']  = [
-    {
-        'CLASS' : 'flask_smal2.sp.idphandler.IdPHandler',
-        'OPTIONS' : {
-            'display_name': 'KeyCloak',
-            'entity_id' : 'http://localhost:8080/realms/TestRealm',
-            'sso_url': '',
-            'slo_url' : '',
-            'certificate' : certificate_from_file('cert2.pem')
+# app.config['SAML2_IDENTITY_PROVIDERS']  = [
+#     {
+#         'CLASS' : 'flask_smal2.sp.idphandler.IdPHandler',
+#         'OPTIONS' : {
+#             'display_name': 'KeyCloak',
+#             'entity_id' : 'http://localhost:8080/realms/TestRealm',
+#             'sso_url': 'http://localhost:8080/realms/TestRealm/protocol/saml',
+#             'slo_url' : 'http://localhost:8080/realms/TestRealm/protocol/saml',
+#             'certificate' : certificate_from_file('keys\idp-certificate.pem')
 
-        } 
-    }
-]
+#         } 
+#     }
+# ]
 
 
 
 
 @app.route("/")
 def index(): 
-    return "Welcome Home"
+    return "Hello world"
+    
+    # if sp.is_user_logged_in():
+    #     auth_data = sp.get_auth_data_in_session()
 
-@app.route('/dashboard')
-@sp.login_required
-def dashboard():
-    return 'You ar logged in using SAML!'
+    #     message = f''' 
+    #         <p> You are logged in as <strong>{auth_data.nameid}</strong>. 
+    #         The Idp sent back the following attributes:<p> '''
+        
+
+    #     logout_url = url_for('flask_saml2_sp.logout')
+    #     logout = f'< form action = "{logout_url}" method="POST"><input type ="submit" value="Log out'
+
+    #     return message + logout
+    
+    # else:
+    #     message = '<p> You are logged out.</p>'
+    #     login_url = url_for('flask_saml2_sp.login')
+    #     link = f'<p><a href="{login_url}"> Log in to continue</a></p>'
+
+    #     return message + link
+    
+
+#blueprint
+#app.register_blueprint(sp.create_blueprint(), url_prefix='/saml/')
+
+
+#@app.route('/dashboard')
+#@sp.login_required
+#def dashboard():
+#    return 'You ar logged in using SAML!'
--- readme.md
+++ readme.md
@@ -1,6 +1,8 @@
 
 
-
+# Library I try to use in this project
+1. Python-saml : Wheel Error
+2. Flask-saml2: It is no longer maintained, which leads to dependency issues
 
 
 
@@ -24,3 +26,11 @@
     - Name ID format: The format for the NameID sent in SAML responses (email, username)
       - NameID is a fundemental element in SAML assertions. It represents the principal (user) in the context of the SP. UserId shared between IDP and SP. FNIDFormat when Enabled the IDP enforces the specified NamedID format in all SAML assertions sent to the client. When Disanled: NameID is dertermined dynamically or defaults to the SP's requested format
     
+
+    Keycloak generate a certificat for  each client. To download that certificate, you have 3 choice for format:
+      - JKS : Java keystore
+      - PKCS12 : 
+      - BCFKS : 
+    I download a pkcs keystore file . From that file I was able to get the key but not the certificate because newer versions of OpenSSL don't handle older encryption algorithms. HAd to try different approach.
+    I just copy and paste certificate from keycloak
+
